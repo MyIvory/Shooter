84d43f283c84db4169aa09b02a143c4551e16957
MyIvory main <pavel.sherban@hotmail.com>
2022-12-24 17:07:33 +0200

    hello commit

--- src/App.js
+++ src/App.js
@@ -2,9 +2,8 @@ import "./App.css";
 
 function App() {
   return <div className="App">
-    <h1>1111111</h1>
-  <h1>2222222222</h1>
-  <h1>3333333333</h1>
+    <h1>Hello world</h1>
+  
   </div>;
 }
 
