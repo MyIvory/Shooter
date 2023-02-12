ea9fb7aa34a4db961135ffaeb072991f72523940
MyIvory main <pavel.sherban@hotmail.com>
2023-02-12 14:20:20 +0200

    selection of substrings in the search result

--- src/components/body/wiki/pages/search_result/search_result.jsx
+++ src/components/body/wiki/pages/search_result/search_result.jsx
@@ -5,15 +5,51 @@ import { useTranslation } from "react-i18next";
 const SearchResult = (props) => {
   const [t, i18n] = useTranslation();
 
+  //console.log(props.searchPage);
   return (
     <div id="search-result-field">
-      <ul>
-        {props.searchPage.map((item, index) => {
+      {props.searchPage.map((item, index) => {
+        if (item.sub) {
+          let s = strong(item.text, item.sub);
+          console.log(s);
+          return (
+            <div>
+              {s.map((it, index) => {
+                console.log(`index->${index}`)
+                //console.log(it)
+                console.log(`s.len->${s.length}`)
+                if (index != s.length-1) {
+                  return (
+                    <span>
+                      {it}
+                      <strong>{item.sub}</strong>
+                    </span>
+                  );
+                } else {
+                  return <span>{it}</span>;
+                }
+              })}
+            </div>
+          );
+        } else {
           return <li key={index}>{item.text}</li>;
-        })}
-      </ul>
+        }
+      })}
     </div>
   );
 };
 
 export default SearchResult;
+function strong(str, sub) {
+  let posArr = [];
+  let pos = -1;
+  let start = 0;
+  while ((pos = str.indexOf(sub, pos + 1)) != -1) {
+    posArr.push(str.substring(start, pos));
+    start = pos + sub.length;
+  }
+  if (start != str.length) {
+    posArr.push(str.substring(start));
+  }
+  return posArr;
+}
--- src/components/header/controls/search/search.jsx
+++ src/components/header/controls/search/search.jsx
@@ -1,56 +1,56 @@
-import s from "./search.module.css";
-import { useRef, useState, useEffect } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
-import { AudioOutlined } from "@ant-design/icons";
-import { Input, Space } from "antd";
-import { useTranslation } from "react-i18next";
-import { FaSearch } from "react-icons/fa";
-import { useNavigate } from "react-router";
-
-const SearchField = (props) => {
-  // console.log(props)
-  const [t, i18n] = useTranslation();
-  const navigate = useNavigate();
-  const [saerchTern, setSearchTern] = useState("");
-  const [translate, setTranslate] = useState([]);
-  const ft = translate.filter((item) => {
-    return item.text.toLowerCase().includes(saerchTern.toLowerCase());
-  });
-
-  useEffect(() => {
-    ft.length>0?props.updateState(ft):props.updateState([{text:"Not found"}])
-  }, [saerchTern]);
-  return (
-    <div className={s.main}>
-      <input
-        type="text"
-        className={s.input}
-        autoComplete="off"
-        placeholder="Search in Wiki"
-        value={saerchTern}
-        onChange={(e) => {
-          setSearchTern(e.target.value);
-          navigate("/wiki/search-result/");
-          if (e.target.value === "") {
-            setTranslate([{text:"Input search"}]);
-          } else {
-            setTranslate(cloneObj(t("search", { returnObjects: true })));
-          }
-        }}
-      ></input>
-      <FaSearch className={s.icon} aria-hidden="true" />
-    </div>
-  );
-};
-export default SearchField;
-
-function cloneObj(obj) {
-  let cloneArr = [];
-  for (let key in obj) {
-    for (let k in obj[key]) {
-      cloneArr.push({ category: key, paragrath: k, text: obj[key][k] });
-    }
-  }
-  return cloneArr;
+import s from "./search.module.css"; 
+import { useRef, useState, useEffect } from "react"; 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; 
+import { faSearch } from "@fortawesome/free-solid-svg-icons"; 
+import { AudioOutlined } from "@ant-design/icons"; 
+import { Input, Space } from "antd"; 
+import { useTranslation } from "react-i18next"; 
+import { FaSearch } from "react-icons/fa"; 
+import { useNavigate } from "react-router"; 
+ 
+const SearchField = (props) => { 
+  // console.log(props) 
+  const [t, i18n] = useTranslation(); 
+  const navigate = useNavigate(); 
+  const [saerchTern, setSearchTern] = useState(""); 
+  const [translate, setTranslate] = useState([]); 
+  const ft = translate.filter((item) => { 
+    return item.text.toLowerCase().includes(saerchTern.toLowerCase()); 
+  }); 
+ 
+  useEffect(() => { 
+    ft.length>0?props.updateState(ft):props.updateState([{text:"Not found"}]) 
+  }, [saerchTern]); 
+  return ( 
+    <div className={s.main}> 
+      <input 
+        type="text" 
+        className={s.input} 
+        autoComplete="off" 
+        placeholder="Search in Wiki" 
+        value={saerchTern} 
+        onChange={(e) => { 
+          setSearchTern(e.target.value); 
+          navigate("/wiki/search-result/"); 
+          if (e.target.value === "") { 
+            setTranslate([{text:"Input search"}]); 
+          } else { 
+            setTranslate(cloneObj(t("search", { returnObjects: true }),e.target.value)); 
+          } 
+        }} 
+      ></input> 
+      <FaSearch className={s.icon} aria-hidden="true" /> 
+    </div> 
+  ); 
+}; 
+export default SearchField; 
+ 
+function cloneObj(obj,sub) { 
+  let cloneArr = []; 
+  for (let key in obj) { 
+    for (let k in obj[key]) { 
+      cloneArr.push({ category: key,sub:sub, paragrath: k, text: obj[key][k] }); 
+    } 
+  } 
+  return cloneArr; 
 }
