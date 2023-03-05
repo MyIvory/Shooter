1a5342f49df80a9942208fc665cccd22a11dbd44
MyIvory <pavel.sherban@hotmail.com>
2023-03-05 13:55:02 +0300

    +

--- src/elements/edit_modal/edit_modal.jsx
+++ src/elements/edit_modal/edit_modal.jsx
@@ -1,51 +1,62 @@
-import { Button, Input, message, Modal } from 'antd';
-import $ from "jquery"
-import { useTranslation } from 'react-i18next';
-import { useLocation } from 'react-router-dom';
+import { Button, Input, message, Modal } from "antd";
+import $ from "jquery";
+import { useTranslation } from "react-i18next";
+import { useLocation } from "react-router-dom";
 
 const { TextArea } = Input;
 
-
 const EditModal = (props) => {
-    const { t } = useTranslation();
-    const loc = useLocation()
-    return (
-        <>
-            <Modal title={`${props.data.header} ${ t(`search.${loc.pathname.split("/").pop()}.title`)}`}
-                open={props.isModalOpen}
-                closable={false}
-                footer={[
-                    <Button key="cancel" onClick={props.closeModal}>
-                        {props.data.cancel_but}
-                    </Button>,
-                    <Button key="clear" onClick={() => {
-                        $("#edit_text_area").val("")
-                    }}>
-                        {props.data.clear_but}
-                    </Button>,
-                    <Button
-                        key="send"
-                        type="primary"
-                        onClick={() => {
-                            let val = $("#edit_text_area").val()                         
-                            if (val) {
-                                console.log(`SEND_MESSAGE->${val}`)  
-                                props.closeModal()
-                                message.info(props.data.message_ok)
-                            }else{
-                                message.info(props.data.message_fail)
-                            }
-
-                        }}
-                    >
-                        {props.data.send_but}
-                    </Button>,
-                ]}
-            >
-                <TextArea id="edit_text_area" rows={8} size='large'></TextArea>
-            </Modal>
-        </>
-    )
-}
+  const { t } = useTranslation();
+  const loc = useLocation();
+  return (
+    <>
+      <Modal
+        title={`${props.data.header} ${t(
+          `search.${loc.pathname.split("/").pop()}.title`
+        )}`}
+        open={props.isModalOpen}
+        closable={false}
+        footer={[
+          <Button key="cancel" onClick={props.closeModal}>
+            {props.data.cancel_but}
+          </Button>,
+          <Button
+            key="clear"
+            onClick={() => {
+              $("#edit_text_area").val("");
+            }}
+          >
+            {props.data.clear_but}
+          </Button>,
+          <Button
+            key="send"
+            type="primary"
+            onClick={() => {
+              let val = $("#edit_text_area").val();
+              if (val) {
+                $.ajax({
+                  type: "POST",
+                  url: "http://localhost/testserver/send_mail.php",
+                  data: {form_data:val},
+                  success: function (data) {
+                   console.log(data);
+                  },
+                });
+                props.closeModal();
+                message.info(props.data.message_ok);
+              } else {
+                message.info(props.data.message_fail);
+              }
+            }}
+          >
+            {props.data.send_but}
+          </Button>,
+        ]}
+      >
+        <TextArea id="edit_text_area" rows={8} size="large"></TextArea>
+      </Modal>
+    </>
+  );
+};
 
 export default EditModal;
