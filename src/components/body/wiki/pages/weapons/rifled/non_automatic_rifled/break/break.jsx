f82333138327e535216c97bb23e5648e31fe0b21
MyIvory <pavel.sherban@hotmail.com>
2023-03-22 22:17:03 +0300

    +++

--- src/components/body/wiki/pages/weapons/rifled/non_automatic_rifled/break/break.jsx
+++ src/components/body/wiki/pages/weapons/rifled/non_automatic_rifled/break/break.jsx
@@ -1,16 +1,14 @@
-import React, { Suspense } from 'react';
-import { Dodecahedron, OrbitControls } from '@react-three/drei';
-import { RifledBoltModel } from '../../../../../../../../elements/rifled_bolt_model/rifled_bolt_model';
-import { Model } from '../../../../../../../../elements/rifled_bolt_model/rifled_bolt_model';
+import React, { Suspense } from "react";
+import { Dodecahedron, OrbitControls } from "@react-three/drei";
+import { RifledBoltModel } from "../../../../../../../../elements/rifled_bolt_model/rifled_bolt_model";
+import { Model } from "../../../../../../../../elements/rifled_bolt_model/rifled_bolt_model";
 
-const RifledBreakAction = props => {
-    return (
-        <div>
-           
-        </div>
-    );
+const RifledBreakAction = (props) => {
+  return (
+    <div style={{width:"100%"}}>
+     
+    </div>
+  );
 };
-
-
 
 export default RifledBreakAction;
